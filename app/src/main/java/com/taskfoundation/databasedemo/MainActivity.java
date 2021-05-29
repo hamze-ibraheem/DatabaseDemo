a4170f4a2edd08ba863c8985cddb10329c6a0caa
Hamza Al-Sarsour <hamze.85@live.com>
2021-05-29 23:27:02 +0300

    setup SQlite database with tables and records.

--- app/src/main/java/com/taskfoundation/databasedemo/MainActivity.java
+++ app/src/main/java/com/taskfoundation/databasedemo/MainActivity.java
@@ -2,8 +2,12 @@ package com.taskfoundation.databasedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.security.spec.ECField;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +16,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SQLiteDatabase database = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
-        
+        try {
+            SQLiteDatabase database = this.openOrCreateDatabase("Events", MODE_PRIVATE, null);
+
+            database.execSQL("CREATE TABLE IF NOT EXISTS events (name VARCHAR, year INT(4))");
+
+            database.execSQL("INSERT INTO users(name,year) VALUES('Ramadan',2021)");
+            database.execSQL("INSERT INTO users(name,year) VALUES('Eid AL-Fetr',2021)");
+
+            Cursor cursor = database.rawQuery("SELECT * FROM events", null);
+
+            int nameIndex = cursor.getColumnIndex("name");
+            int yearIndex = cursor.getColumnIndex("year");
+
+            cursor.moveToFirst();
+
+            while (!cursor.isAfterLast()) {
+                Log.i("event name:", cursor.getString(nameIndex));
+                Log.i("event year:", cursor.getString(yearIndex));
+
+                cursor.moveToNext();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
