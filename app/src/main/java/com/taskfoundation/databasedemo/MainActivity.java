58434f1bf871bd2d18321b2484645fecc89ab5f9
Hamza Al-Sarsour <hamze.85@live.com>
2021-05-29 23:48:47 +0300

    fix bugs

--- app/src/main/java/com/taskfoundation/databasedemo/MainActivity.java
+++ app/src/main/java/com/taskfoundation/databasedemo/MainActivity.java
@@ -19,21 +19,24 @@ public class MainActivity extends AppCompatActivity {
         try {
             SQLiteDatabase database = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
 
-            database.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+            database.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3), id INTEGER PRIMARY KEY)");
 
             database.execSQL("INSERT INTO users(name,age) VALUES('Hamza',36)");
             database.execSQL("INSERT INTO users(name,age) VALUES('Ahmed',37)");
+            database.execSQL("DELETE FROM users WHERE name='Ahmed'");
 
-            Cursor cursor = database.rawQuery("SELECT * FROM users WHERE age > 18", null);
+            Cursor cursor = database.rawQuery("SELECT * FROM users WHERE id = 2 AND age > 18 AND name LIKE '%a%' LIMIT 1", null);
 
             int nameIndex = cursor.getColumnIndex("name");
             int ageIndex = cursor.getColumnIndex("age");
+            int idIndex = cursor.getColumnIndex("id");
 
             cursor.moveToFirst();
 
             while (!cursor.isAfterLast()) {
                 Log.i("name:", cursor.getString(nameIndex));
                 Log.i("age:", Integer.toString(cursor.getInt(ageIndex)));
+                Log.i("id:", Integer.toString(cursor.getInt(idIndex)));
 
                 cursor.moveToNext();
             }
